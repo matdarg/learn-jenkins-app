4044d7a444f874c1a2c81e7e2b1057bbc5d30650
Mateusz Dragan <matdarg@gmail.com>
2025-08-22 09:19:39 +0200

    update run-all script

--- run_all.ps1
+++ run_all.ps1
@@ -5,30 +5,16 @@ $dockerPath = "$jenkinsProjectPath\install-jenkins-docker"
 # variable for the path to the Jenkins home directory
 $jenkinsAppPath = "$jenkinsProjectPath\learn-jenkins-app"
 
-# Run docker compose down in the background
-Start-Job -ScriptBlock {
-    try {
-        Set-Location $dockerPath
-        docker compose down
-        docker compose up -d
-        Start-Process "chrome.exe" "http://localhost:8080"
-    } catch {
-        Write-Error "Docker compose or Chrome failed: $_"
-    }
-}
+# Run docker compose down and up in a new PowerShell window
+Start-Process "powershell.exe" -ArgumentList "cd $dockerPath; docker compose down; docker compose up -d"
 
-# Open Chrome to the Jenkins Udemy course
+# npm start start-process in a new PowerShell window without opening the browser
+Start-Process "powershell.exe" -ArgumentList "cd $jenkinsAppPath; npm start -- --no-browser"
+
+# Open chrome to the Jenkins Udemy course and react app and Jenkins server
 Start-Process "chrome.exe" "https://cognizant.udemy.com/course/jenkins-ci-cd-pipelines-devops-for-beginners"
-
-
 Start-Process "chrome.exe" "http://localhost:3000"
+Start-Process "chrome.exe" "http://localhost:8080"
 
 # start vscode in the Jenkins app directory
 Start-Process "code" -ArgumentList $jenkinsAppPath
-
-# npm start start-process in a new PowerShell window without opening the browser
-Start-Process "powershell.exe" -ArgumentList "cd $jenkinsAppPath; npm start"
-# get jobs to wait for completion
-$jobs = Get-Job
-# wait for all jobs to complete
-Wait-Job -Job $jobs
